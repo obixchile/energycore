e2094e79a0904f7255c3118b2e0c00683a6c5c84
obixchile <obixchile@gmail.com>
2026-03-19 20:00:29 -0300

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default { content: ['./index.html','./src/**/*.{js,ts,jsx,tsx}'], theme: { extend: { colors: { enel: { dark: '#0d1b2a', sidebar: '#111827' } } } }, plugins: [] }
